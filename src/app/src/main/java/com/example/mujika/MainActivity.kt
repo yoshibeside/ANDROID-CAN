85a76cf81f8da47aa89f1b054260cd933be05c20
Christine Hutabarat <13520005@std.stei.itb.ac.id>
2023-02-23 17:04:53 +0700

    fix: hide menu item

--- src/app/src/main/java/com/example/mujika/MainActivity.kt
+++ src/app/src/main/java/com/example/mujika/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.mujika
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
-import android.widget.FrameLayout
+import android.view.Menu
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mujika.databinding.ActivityMainBinding
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.Toolbar))
+        supportActionBar?.title = ""
+
         changeFragment("Twibbon")
+
         binding.bottomNavigation.setOnItemSelectedListener {
                 when(it.itemId){
                 R.id.menu -> changeFragment("Menu")
@@ -33,22 +32,18 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+    @SuppressLint("RestrictedApi", "ResourceType")
     public fun changeFragment(tag: String){
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         var fragment = fragmentManager.findFragmentByTag(tag)
 
-        setSupportActionBar(findViewById(R.id.Toolbar))
-        val tv1: TextView = findViewById(R.id.toolbar_title)
-        tv1.text = tag
-        if (tag == "Menu") {
-            tv1.text = ""
-        }
+        changeToolbar(tag)
 
         if (fragment == null) {
             // create new instance according to the tag given
-            val newFragment = when (tag) {
+            fragment = when (tag) {
                 "Twibbon" -> TwibbonFragment()
                 "Menu" -> MenuFragment()
                 "Cabang Restoran" -> CabangRestoranFragment()
@@ -58,15 +53,58 @@ class MainActivity : AppCompatActivity() {
                 else -> throw IllegalArgumentException("Fragment type undefined")
             }
 
-            fragmentTransaction.add(R.id.container, newFragment, tag)
+            fragmentTransaction.add(R.id.container, fragment, tag)
             Log.d("TAG", "New $tag created")
         } else {
             // existing instance of fragment found
             fragmentTransaction.replace(R.id.container, fragment, tag)
             Log.d("TAG", "Reusing existing $tag")
         }
+
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
+
+    }
+
+    private fun changeToolbar(tag: String) {
+        val toolbarTitle = findViewById<TextView>(R.id.toolbar_title)
+
+        if (tag == "Menu") {
+            toolbarTitle.text = ""
+            supportActionBar?.title = tag
+            showMenu()
+        } else {
+            toolbarTitle.text = tag
+            supportActionBar?.title = ""
+            hideMenu()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val view = supportFragmentManager.findFragmentById(R.id.container)
+        changeToolbar(view?.tag.toString())
+    }
+    private var menu: Menu? = null
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option_bar, menu)
+        this.menu = menu
+        hideMenu()
+        return true
+    }
+
+    fun hideMenu() {
+        menu?.let {
+            it.findItem(R.id.menu_main_setting)?.isVisible = false
+            it.findItem(R.id.menu_main_setting2)?.isVisible = false
+        }
+    }
+
+    fun showMenu() {
+        menu?.let {
+            it.findItem(R.id.menu_main_setting)?.isVisible = true
+            it.findItem(R.id.menu_main_setting2)?.isVisible = true
+        }
     }
 
 }
