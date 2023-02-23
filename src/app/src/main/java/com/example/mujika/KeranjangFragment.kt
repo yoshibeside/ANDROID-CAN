a601ce5fd817e619b46799c6169cb33d4812d32f
angelws <13520097@std.stei.itb.ac.id>
2023-02-23 08:34:00 +0700

    feat: add keranjang Fragment and adapter

--- src/app/src/main/java/com/example/mujika/KeranjangFragment.kt
+++ src/app/src/main/java/com/example/mujika/KeranjangFragment.kt
@@ -1,19 +1,63 @@
 package com.example.mujika
 
 import android.os.Bundle
+import android.view.*
+import android.widget.SearchView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.launch
+import retrofit.adaptor.KeranjangAdaptor
+import retrofit.adaptor.MenuAdapter
+import retrofit.api.RetrofitClient
+import retrofit.model.ListMenu
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import roomdb.AppDatabase
+import roomdb.KeranjangDao
+import roomdb.MenuDatabase
+import roomdb.TypeMenu
 
 class KeranjangFragment : Fragment() {
 
+    private lateinit var keranjangDao : KeranjangDao
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_keranjang, container, false)
+        // Inflate the layout for this fragment
+        val appDb = AppDatabase.getDatabase(requireContext())
+        keranjangDao = appDb.keranjangDao()
         setHasOptionsMenu(false)
-        return inflater.inflate(R.layout.fragment_keranjang, container, false)
+        (activity as AppCompatActivity).supportActionBar?.title = ""
+
+        val recyclerView = view?.findViewById<RecyclerView>(R.id.recycler_view_keranjang)
+        var list_menu_keranjang = ArrayList<MenuDatabase>()
+
+        lifecycleScope.launch() {
+            val tempKeranjang = keranjangDao.findMenu()
+            list_menu_keranjang = ArrayList(tempKeranjang)
+        }
+        if (!list_menu_keranjang.isNullOrEmpty()) {
+            println("Masuk ya disini??")
+            println(list_menu_keranjang)
+            val adapter = KeranjangAdaptor(list_menu_keranjang, keranjangDao, object: KeranjangAdaptor.OnDataUpdateListener {
+                override fun onDataUpdate() {
+                    val updateTotalPrice = keranjangDao.getTotalPrice()
+                    val textTotalPrice = String.format("Rp %d", updateTotalPrice)
+                    view?.findViewById<TextView>(R.id.harga_total_pembayaran)?.text = textTotalPrice
+                }
+            })
+            recyclerView?.layoutManager = LinearLayoutManager(activity)
+            recyclerView?.adapter = adapter
+        }
+        return view
     }
+
+
 }
---
+++ src/app/src/main/java/retrofit/adaptor/KeranjangAdaptor.kt
@@ -0,0 +1,80 @@
+package retrofit.adaptor
+
+
+import android.view.LayoutInflater
+import retrofit.model.MenuItemStuff
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mujika.R
+import roomdb.KeranjangDao
+import roomdb.MenuDatabase
+
+class KeranjangAdaptor (private var list: ArrayList<MenuDatabase>, private val keranjangDao: KeranjangDao, private var onDataUpdateListener: OnDataUpdateListener): RecyclerView.Adapter<KeranjangAdaptor.KeranjangViewHolder>(){
+    inner class KeranjangViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        fun bind(menu: MenuDatabase) {
+            val menuName = itemView.findViewById<TextView>(R.id.menu_keranjang_name)
+            val price = itemView.findViewById<TextView>(R.id.fullprice)
+            val amount = itemView.findViewById<TextView>(R.id.amount_keranjang)
+            val calculationResult = (menu.amount!! * menu.price!!)
+
+            with(itemView){
+                val textForPrice = String.format("Rp %d", calculationResult);
+                println("diatas ini jawabannya. kenapa ngga mau bind ya?")
+                menuName.text= menu.name_menu
+                price.text= textForPrice
+                amount.text= menu.amount.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : KeranjangViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_keranjang, parent, false)
+        return KeranjangViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = list.size;
+
+    override fun onBindViewHolder(holder:KeranjangViewHolder, position: Int) {
+
+        holder.bind(list[position])
+
+        println("terjalnaknan onBindViewHolder")
+
+        holder.itemView.findViewById<Button>(R.id.plus_keranjang).setOnClickListener{
+            val name_menu_updating = list.get(position).name_menu!!
+            val updated_amount = (holder.itemView.findViewById<TextView>(R.id.amount_keranjang).text.toString()).toInt() + 1
+            val string_price = String.format("Rp %d", updated_amount* list.get(position).price!!)
+            holder.itemView.findViewById<TextView>(R.id.amount_keranjang).text = updated_amount.toString()
+            holder.itemView.findViewById<TextView>(R.id.fullprice).text = string_price
+            keranjangDao.update(name_menu_updating, updated_amount)
+            onDataUpdateListener.onDataUpdate()
+        }
+
+        holder.itemView.findViewById<Button>(R.id.minus_keranjang).setOnClickListener{
+            val name_menu_updating = list.get(position).name_menu!!
+            val updated_amount =  (holder.itemView.findViewById<TextView>(R.id.amount_keranjang).text.toString()).toInt() - 1
+            val string_price = String.format("Rp %d", updated_amount* list.get(position).price!!)
+            println("updated amountnya adalah segini " + updated_amount)
+            if (updated_amount ==  0) {
+                println("malah masuk ke 0")
+                list.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, list.size)
+            } else {
+                holder.itemView.findViewById<TextView>(R.id.amount_keranjang).text = updated_amount.toString()
+                holder.itemView.findViewById<TextView>(R.id.fullprice).text = string_price
+            }
+            keranjangDao.update(name_menu_updating, updated_amount)
+            onDataUpdateListener.onDataUpdate()
+        }
+        onDataUpdateListener.onDataUpdate()
+    }
+
+    interface OnDataUpdateListener {
+        fun onDataUpdate()
+    }
+
+}
